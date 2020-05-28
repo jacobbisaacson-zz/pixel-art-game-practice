e6eb0e23212361528559894befd7cd0d8db06bb6
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-05-28 13:52:43 -0500

    changed to wasd

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ function animate() {
     player.height, player.x, player.y, player.width * 2, player.height * 2)
   // can scale with player height or width * 2.
   movePlayer()
+  handlePlayerFrame()
   requestAnimationFrame(animate)
 }
 
@@ -43,34 +44,43 @@ animate()
 
 window.addEventListener("keydown", function(e) {
   keys[e.keyCode] = true
-  // console.log(keys);
+  player.moving = true
+  console.log(keys);
 })
 
 window.addEventListener("keyup", function(e) {
   delete keys[e.keyCode]
+  player.moving = false
 })
 
 function movePlayer() {
-  //if up key is pressed, and player's position on the window is less than height
+  //if up key is pressed, AND player's position on the window is less than height
   // then move the player up when key is pressed
   // AND change their position (sprite) to face up (3)
   // do the same for left, right, down
-  if(keys[38] && player.y > 100){
+  if(keys[87] && player.y > 100){
     player.y -= player.speed
     player.frameY = 3
   }
-  if(keys[37] && player.x > 0){
+  if(keys[65] && player.x > 0){
     player.x -= player.speed
     player.frameY = 1
   }
-  if(keys[37] && player.x > 0){
-    player.x -= player.speed
-    player.frameY = 1
+  if(keys[83] && player.y < canvas.height - player.height){
+    player.y += player.speed
+    player.frameY = 0
   }
-  if(keys[37] && player.x > 0){
-    player.x -= player.speed
-    player.frameY = 1
+  if(keys[68] && player.x < canvas.width - player.width){
+    player.x += player.speed
+    player.frameY = 2
   }
+}
+
+//to show legs moving animation
+//AND if player is moving
+function handlePlayerFrame() {
+  if(player.frameX < 3 && player.moving) player.frameX++
+    else player.frameX = 0
 }
 
 
